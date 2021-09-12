d3caa32b6484ecf72133e536d6f952babf6688ab
Himanshu Judge <jhimanshu9922@gmail.com>
2021-09-12 15:49:36 +0530

    Spring Rest Application

--- Spring Assignment/6.bookapp-rest/src/main/java/com/bookapp/web/controller/BookController.java
+++ Spring Assignment/6.bookapp-rest/src/main/java/com/bookapp/web/controller/BookController.java
@@ -3,6 +3,8 @@ package com.bookapp.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,37 +18,41 @@ import com.bookapp.web.entities.Book;
 
 @RestController
 public class BookController {
-	
+
 	private BookService bookService;
 
 	@Autowired
 	public void setBookService(BookService bookService) {
 		this.bookService = bookService;
 	}
-	
+
 	@GetMapping("/book")
-	public List<Book> getAllBooks(){
+	public ResponseEntity<List<Book>> getAllBooks() {
 		List<Book> allBooks = bookService.getAllBooks();
-		return allBooks;
+		return ResponseEntity.status(200).body(allBooks);
 	}
-	
-	@GetMapping("/book/{id}")
-	public Book getBookById(@PathVariable(name="id")int id) {
-		return bookService.getBookById(id);
-	}
-	
+
 	@PostMapping("/book")
-	public Book addBook(@RequestBody Book book) {
-		return bookService.addBook(book);
+	public ResponseEntity<Book> addBook(@RequestBody Book book) {
+		bookService.addBook(book);
+		return ResponseEntity.status(201).body(book);
 	}
-	
+
+	@GetMapping("/book/{id}")
+	public ResponseEntity<Book> getBookById(@PathVariable(name = "id") int id) {
+		return ResponseEntity.status(200).body(bookService.getBookById(id));
+	}
+
 	@DeleteMapping("book/{id}")
-	public Book deleteBookById(@PathVariable (name="id") int id) {
-		return bookService.deleteBook(id);
+	public ResponseEntity<String> deleteBookById(@PathVariable(name = "id") int id) {
+		bookService.deleteBook(id);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body("book deleted with id ; "+id);
 	}
-	
+
 	@PutMapping("book/{id}")
-	public Book updateBookById(@PathVariable (name="id")int id,@RequestBody Book book) {
-		return bookService.updateBook(id, book);
+	public ResponseEntity<Book> updateBookById(@PathVariable(name = "id") int id, @RequestBody Book book) {
+		Book updateBook = bookService.updateBook(id, book);
+		System.out.println(updateBook);
+		return ResponseEntity.status(HttpStatus.OK).body(updateBook);
 	}
 }
--- Spring Assignment/6.bookapp-rest/src/main/java/com/bookapp/web/exceptions/BookNotFoundException.java
+++ Spring Assignment/6.bookapp-rest/src/main/java/com/bookapp/web/exceptions/BookNotFoundException.java
@@ -1,5 +1,8 @@
 package com.bookapp.web.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class BookNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
